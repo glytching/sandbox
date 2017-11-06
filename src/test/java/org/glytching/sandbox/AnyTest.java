298af3ffc30733adbcc914f87024ad13d7233ea6
glytchy <colm.mcdonnell@gmail.com>
2017-11-06 10:29:43 +0000

    Push to trigger first build

--- src/test/java/org/glytching/sandbox/AnyTest.java
+++ src/test/java/org/glytching/sandbox/AnyTest.java
@@ -3,6 +3,7 @@ package org.glytching.sandbox;
 import org.junit.Test;
 
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
 import static org.junit.Assert.assertThat;
 
 public class AnyTest {
@@ -10,5 +11,10 @@ public class AnyTest {
     @Test
     public void aTest() {
         assertThat((1+1), is(2));
+    }
+
+    @Test
+    public void bTest() {
+        assertThat((1+1), not(3));
     }
 }
