6560ec71b9a70af90423dcfa334f4fb2a1128942
colm.mcdonnell <colm.mcdonnell@gmail.com>
2018-08-17 11:22:28 +0100

    Added StrictConfigurationWarningStatusListener and a Mongo test showing persistence of an embedded document

--- src/test/java/org/glytching/sandbox/mongo/MongoReadWriteTest.java
+++ src/test/java/org/glytching/sandbox/mongo/MongoReadWriteTest.java
@@ -149,6 +149,45 @@ public class MongoReadWriteTest {
             assertThat(ex.getWriteErrors().get(0).getCode(), is(11000));
             assertThat(ex.getWriteErrors().get(0).getMessage(), startsWith("E11000 duplicate key error"));
         }
+    }
 
+    @Test
+    public void canPersistDocumentWithEmbeddedSubDocument() {
+        MongoClient mongoClient = new MongoClientFactory().create();
+
+        MongoCollection<Document> collection = mongoClient.getDatabase("stackoverflow").getCollection("subdocument");
+
+        Document metadata = new Document("_class", "eu.ohim.rcd.efi.DocumentEntity")
+                .append("_id", "RCD201800000001024-0002-0000")
+                .append("provisionalId", "RCD201800000001024-0002")
+                .append("designApplicationId", "RCD201800000001024")
+                .append("documentType", "PROTECTED_VIEW")
+                .append("rcdProtected", true);
+
+        Document document = new Document("filename", "View000.jpg")
+                .append("aliases", null)
+                .append("chunkSize", 261120L)
+                .append("uploadDate", new Date())
+                .append("length", 29179L)
+                .append("contentType", "image/jpeg")
+                .append("md5", "1402721cc080174ac6ae9c1d51eb68ab")
+                .append("metadata", metadata);
+
+        collection.insertOne(document);
+
+        Document insertedDocument = collection.find(Filters.eq("filename", "View000.jpg")).first();
+
+        assertThat(insertedDocument.containsKey("filename"), is(true));
+        assertThat(insertedDocument.getString("filename"), is(document.getString("filename")));
+        assertThat(insertedDocument.getLong("chunkSize"), is(document.getLong("chunkSize")));
+
+        assertThat(insertedDocument.containsKey("metadata"), is(true));
+        Document subDocument = (Document) insertedDocument.get("metadata");
+        assertThat(subDocument.getString("_id"), is(metadata.get("_id")));
+        assertThat(subDocument.getString("_class"), is(metadata.get("_class")));
+        assertThat(subDocument.getString("provisionalId"), is(metadata.get("provisionalId")));
+        assertThat(subDocument.getString("designApplicationId"), is(metadata.get("designApplicationId")));
+        assertThat(subDocument.getString("documentType"), is(metadata.get("documentType")));
+        assertThat(subDocument.getBoolean("rcdProtected"), is(metadata.get("rcdProtected")));
     }
 }
